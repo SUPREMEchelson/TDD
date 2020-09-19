5787a9f2fb1987e98109ee48446978f0b445295e
Chelson Supreme <chelson.sm@gmail.com>
2020-09-19 23:55:07 +0200

    mise a jour du dossier report mise en place de dataProvider verification du couvrement et remaniment du code

--- src/main/java/fr/p10/miage/rps/model/RockPaperScissors.java
+++ src/main/java/fr/p10/miage/rps/model/RockPaperScissors.java
@@ -3,14 +3,17 @@ import org.testng.annotations.Parameters;
 
 public class RockPaperScissors {
     Result play(RPSEnum p1, RPSEnum p2){
-        if(p1 == RPSEnum.PAPER && p2 == RPSEnum.ROCK) {return Result.WIN;}
 
-        if(p1 == RPSEnum.PAPER && p2 == RPSEnum.PAPER){
-            return Result.WIN;
-        }
-        if(p1 == RPSEnum.PAPER && p2 == RPSEnum.SCISSORS){
-            return Result.LOST;
-        }
+        if(p1 == RPSEnum.PAPER && p2 == RPSEnum.ROCK) {return Result.WIN;}
+        if(p1 == RPSEnum.ROCK && p2 == RPSEnum.SCISSORS){ return Result.WIN;}
+        if(p1 == RPSEnum.SCISSORS && p2 == RPSEnum.PAPER) { return Result.WIN;}
+
+        if(p1 == RPSEnum.PAPER && p2 == RPSEnum.PAPER){return Result.TIE;}
+        if(p1 == RPSEnum.ROCK && p2 == RPSEnum.ROCK){return Result.TIE;}
+        if(p1 == RPSEnum.SCISSORS && p2 == RPSEnum.SCISSORS){return Result.TIE;}
+
+        if(p1 == RPSEnum.PAPER && p2 == RPSEnum.SCISSORS){return Result.LOST;}
+
         return Result.LOST;
 
     }
--- src/test/java/fr/p10/miage/rps/model/RockPaperScissorsTest.java
+++ src/test/java/fr/p10/miage/rps/model/RockPaperScissorsTest.java
@@ -1,6 +1,5 @@
 package fr.p10.miage.rps.model;
-import fr.p10.miage.rps.model.RPSEnum;
-import org.testng.annotations.Parameters;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
@@ -23,11 +22,51 @@ RockPaperScissors rps;
     }
     @Test
     public void testTieplay(){
-        assertEquals(rps.play(RPSEnum.PAPER,RPSEnum.PAPER), Result.WIN);
+        assertEquals(rps.play(RPSEnum.PAPER,RPSEnum.PAPER), Result.TIE);
     }
     @Test
     public void testLostPlay(){
         assertEquals(rps.play(RPSEnum.PAPER,RPSEnum.SCISSORS),Result.LOST);
     }
 
+    @Test(dataProvider = "WinData")
+    public void wintest(RPSEnum p1, RPSEnum p2)
+    {
+        assertEquals((rps.play(p1,p2)),Result.WIN);
+    }
+
+    @DataProvider(name="WinData")
+    public Object[][] getData1()
+    {
+        Object[][] data1 = { {RPSEnum.PAPER,RPSEnum.ROCK}, {RPSEnum.ROCK,RPSEnum.SCISSORS}, {RPSEnum.SCISSORS,RPSEnum.PAPER}};
+       return data1;
+    }
+
+
+    @Test(dataProvider = "TieData")
+    public void tietest(RPSEnum p1, RPSEnum p2)
+    {
+        assertEquals((rps.play(p1,p2)),Result.TIE);
+    }
+
+    @DataProvider(name="TieData")
+    public Object[][] getData2()
+    {
+        Object[][] data2 = { {RPSEnum.PAPER,RPSEnum.PAPER}, {RPSEnum.ROCK,RPSEnum.ROCK}, {RPSEnum.SCISSORS,RPSEnum.SCISSORS}};
+        return data2;
+    }
+
+
+    @Test(dataProvider = "LostData")
+    public void losttest(RPSEnum p1, RPSEnum p2)
+    {
+        assertEquals((rps.play(p1,p2)),Result.LOST);
+    }
+
+    @DataProvider(name="LostData")
+    public Object[][] getData3()
+    {
+        Object[][] data3 = { {RPSEnum.PAPER,RPSEnum.SCISSORS}, {RPSEnum.ROCK,RPSEnum.PAPER}, {RPSEnum.SCISSORS,RPSEnum.ROCK}};
+        return data3;
+    }
 }
